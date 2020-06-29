5883393c92f3e67ba08aac38075ac3bd75b6214c
nht1206 <nghuutho74@gmail.com>
2020-06-29 14:50:43 +0700

    remove unnecessary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { HomeComponent } from './home/home.component';
 import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
