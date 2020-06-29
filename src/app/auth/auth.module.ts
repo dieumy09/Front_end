6f8813b9f90eb328591e9e2d9f13821cd7884ef2
medivTheguardian <tranvanthuanaacc@gmail.com>
2020-06-29 11:19:48 +0700

    set routing

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    CommonModule
+  ],
+  exports: [
   ]
 })
 export class AuthModule { }
