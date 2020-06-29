0f04b5af1a0c448d260af0a5155b3bab4b5f83f6
PhanMinhHuy <59156011+PhanMinhHuy@users.noreply.github.com>
2020-06-29 11:53:30 +0700

    pots-detail

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { PostDetailComponent } from './components/list-posts/post-detail/post-detail.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PostDetailComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
