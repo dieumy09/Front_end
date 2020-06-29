1ed821f62f1f7b8b3ab331065dc18551a6ee75a2
Your Name <nguyenhanguyen912@gmail.com>
2020-06-29 14:46:00 +0700

    nguyen

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+    declarations: [UserComponent, UserInfoComponent, PostListComponent, PostEditComponent],
+    exports: [
+        UserComponent
+    ],
+    imports: [
+        CommonModule,
+        UserRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class UserModule { }
