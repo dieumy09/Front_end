dca2d328d3aa1a0192baa7f888372f74a4b88be5
Your Name <nguyenhanguyen912@gmail.com>
2020-07-03 16:52:47 +0700

    list post by user DONE

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,28 +2,30 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Post} from '../models/post';
+import {User} from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   private readonly API = 'http://localhost:8080/api/v1/posts';
+  private readonly USER_API = 'http://localhost:8080/api/v1/users';
 
   constructor(private http: HttpClient) { }
 
-  getPosts(): Observable<any> {
-    return this.http.get<any>(this.API);
+  getPosts(): Observable<Post> {
+    return this.http.get<Post>(this.API);
   }
 
-  createPost(post: Post): Observable<any> {
-    return this.http.post<any>(this.API, post);
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.API, post);
   }
 
-  editPost(post: Post, id: number): Observable<any> {
-    return this.http.put(`${this.API}/${id}`, post);
+  editPost(post: Post, id: number): Observable<Post> {
+    return this.http.put<Post>(`${this.API}/${id}`, post);
   }
 
-  deletePost(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.API}/${id}`);
+  deletePost(id: number): Observable<Post> {
+    return this.http.delete<Post>(`${this.API}/${id}`);
   }
 }
