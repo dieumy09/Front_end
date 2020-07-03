b420807de1c5d1c6f65364efe3fe664b2e9b9900
Your Name <nguyenhanguyen912@gmail.com>
2020-07-03 10:06:44 +0700

    user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  id: number;
+  name: string;
+  address: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  avatar: string;
+  status: boolean;
+  activated: boolean;
+}
