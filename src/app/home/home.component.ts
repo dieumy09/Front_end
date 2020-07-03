9cc30604a6eef1a890d0053cec64dc42afb73ec9
nht1206 <nghuutho74@gmail.com>
2020-07-03 21:38:56 +0700

    add list for paging

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
+import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
+    this.postService.getPosts(0).subscribe((next) => {
+      console.log(next);
+    });
   }
-
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,29 +1,30 @@
+import { PostList } from './../models/post-list';
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Post} from '../models/post';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
   private readonly API = 'http://localhost:8080/api/v1/posts';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<any> {
-    return this.http.get<any>(this.API);
+  getPosts(page = 0): Observable<PostList<Post>> {
+    return this.http.get<PostList<Post>>(this.API + '?page=' + page);
   }
 
-  createPost(post: Post): Observable<any> {
-    return this.http.post<any>(this.API, post);
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.API, post);
   }
 
-  editPost(post: Post, id: number): Observable<any> {
-    return this.http.put(`${this.API}/${id}`, post);
+  editPost(post: Post, id: number): Observable<Post> {
+    return this.http.put<Post>(`${this.API}/${id}`, post);
   }
 
-  deletePost(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.API}/${id}`);
+  deletePost(id: number): Observable<Post> {
+    return this.http.delete<Post>(`${this.API}/${id}`);
   }
 }
