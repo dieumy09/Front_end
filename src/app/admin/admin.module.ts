4f737fba1c550f10675d97e19f3f23b3453f4337
nht1206 <nghuutho74@gmail.com>
2020-07-15 11:30:12 +0700

    finish user management UI

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,10 +5,12 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { UserDetailComponent } from './components/user/user-detail/user-detail.component';
 
 
 @NgModule({
-  declarations: [AdminComponent, NavBarComponent, SideBarComponent],
+  declarations: [AdminComponent, NavBarComponent, SideBarComponent, UserListComponent, UserDetailComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
