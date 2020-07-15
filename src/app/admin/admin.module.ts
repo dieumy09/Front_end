43c2b71262608f988e13d8ebdc0d01d869f7836e
nht1206 <nghuutho74@gmail.com>
2020-07-15 09:49:17 +0700

    create admin prototype

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, NavBarComponent, SideBarComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
